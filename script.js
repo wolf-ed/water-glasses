03248117a9a4beaebd2fa68599347d72d8cb6cc2
wolf-ed <wolf7ed@gmail.com>
2022-02-19 23:26:24 +0100

    script

---
+++ script.js
@@ -0,0 +1,19 @@
+const smallCups = document.querySelectorAll('.cup-small');
+const listers = document.getElementById('liters');
+const percentage = document.getElementById('percentage');
+const remained = document.getElementById('liters');
+
+const highlightCups = (idx) => {
+    smallCups.forEach((cup, idx2) => {
+        if (idx2 <= idx) {
+            cup.classList.add('full')
+        } else {
+            cup.classList.remove('full')
+
+        }
+    })
+};
+
+smallCups.forEach((cup, idx) => {
+    cup.addEventListener('click', () => highlightCups(idx));
+})
